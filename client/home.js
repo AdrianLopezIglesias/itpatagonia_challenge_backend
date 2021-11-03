52e0462bc7d4f74022b49e5eda85a3de8e02d467
Adrian Lopez Iglesias <adrianlopeziglesias1@gmail.com>
2021-11-03 16:02:36 -0300

    Empezando proyecto

---
+++ client/home.js
@@ -0,0 +1,31 @@
+import Vue from "vue";
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import App from './App.vue'
+import VueRouter from 'vue-router';
+import routes from './routes';
+Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({ mode: 'history', routes });
+
+new Vue({
+  render: h => h(App),
+  router
+}).$mount('#home');
+
+// let app = new Vue({
+//     el: '#home',
+//     data: {
+//     },
+//     methods: {
+
+// 	},
+// 	components: {
+// 			'home': Pokedex
+// 		},
+// 	mounted() {
+//     }
+// })
---
+++ index.js
@@ -0,0 +1,42 @@
+//START
+const express = require('express')
+const app = express()
+
+//GENERAL PORPOUSE
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+require('dotenv').config()
+
+//SWAGGER
+options = require("./docs/basicInfo")
+const swaggerJsdoc = require("swagger-jsdoc")
+const swaggerUi = require("swagger-ui-express")
+const specs = swaggerJsdoc(options);
+app.use(
+	"/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: false })
+	);
+	
+//ROUTES
+const routes = require('./routes/api')
+app.use('/api', routes);
+app.get('/', (req, res) => res.render('home'))
+app.get('/pokemon/*', (req, res) => res.render('home'))
+
+
+//VIEWS
+const webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+let pug = require("pug")
+let path = require('path')
+app.set(path.join(__dirname, './views'))
+app.set('view engine', 'pug')
+app.use(express.static(__dirname + '/public'));
+
+
+//EXPORT
+module.exports = app; 
